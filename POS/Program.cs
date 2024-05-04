31827f81ebc45391fb8eb7d368f086c272594bca
Aya <137115690+techTitanMaya@users.noreply.github.com>
2024-05-04 16:00:20 +0200

    Add files via upload

---
+++ POS/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Poppel_Order_System.Presentation_Layer;
+using System.Windows.Forms;
+
+namespace Poppel_Order_System
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MDIParent1());
+
+        }
+    }
+}
